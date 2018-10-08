85472ed73200da11b2c9c75282878a39a947f645
Eric Chrobak <eric@ericchrobak.com>
2018-10-08 18:46:31 +0000

    automatically backed up by learn

--- spec/application_integration_spec.rb
+++ spec/application_integration_spec.rb
@@ -13,7 +13,7 @@ describe "GET '/' - Greeting Form" do
 
     expect(page).to have_selector("form")
     expect(page).to have_field(:user_name)
-    # expect(page).to have_text("Yo Eric")
+    expect(page).to have_text("Yo Eric")
   end
 end
 
